172a66ec84a0646a2269649a948495aecc6101ff
Guilherme Higino <guilhermehiginoo@gmail.com>
2025-10-28 19:37:39 -0300

    refactor: remove unused methods from NcmController to streamline code

--- app/Http/Controllers/NcmController.php
+++ app/Http/Controllers/NcmController.php
@@ -3,13 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Ncm;
-use Illuminate\Http\Request;
 
 class NcmController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $ncms = Ncm::orderBy('id', 'asc')->paginate(20);
@@ -17,46 +13,10 @@ class NcmController extends Controller
         return view('ncm.index', compact('ncms'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-    /**
-     * Remove the specified resource from storage.
-     */
 }
