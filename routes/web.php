29850ca37b79e7ca3af9813d5a29f493735cea10
matheus <37665843+wallysom2@users.noreply.github.com>
2025-11-13 17:10:54 -0300

    refactor: simplify welcome route to redirect to login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\{NcmController, ProfileController};
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -10,12 +9,7 @@ Route::get('/', function () {
         return redirect('/classificar');
     }
 
-    return Inertia::render('Welcome', [
-        'canLogin'       => Route::has('login'),
-        'canRegister'    => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion'     => PHP_VERSION,
-    ]);
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
