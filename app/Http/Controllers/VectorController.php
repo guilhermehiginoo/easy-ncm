2ea54148b217e814d8e7fe971f92dc09c30a4df4
Guilherme Higino <guilhermehiginoo@gmail.com>
2025-11-15 18:51:22 -0300

    feat: implement console command for vectorizing NCMs and refactor QdrantService integration

--- app/Http/Controllers/VectorController.php
+++ app/Http/Controllers/VectorController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Ncm;
 use App\Services\{OpenAiService, QdrantService};
 use Throwable;
 
@@ -14,31 +13,25 @@ class VectorController extends Controller
 
     public function vectorizeNcm()
     {
-        $ncms = Ncm::whereNull('embedding_status')
-            ->orWhereIn('embedding_status', ['error', 'pending'])
-            ->lazyById(1000);
+        try {
+            $vector = $this->openAiService->createVector($ncm->description);
 
-        foreach ($ncms as $ncm) {
-            try {
-                $vector = $this->openAiService->createVector($ncm->description);
+            $point = QdrantService::makePoint($ncm->id, $vector, [
+                'ncm_code'    => $ncm->ncm_code,
+                'description' => $ncm->description,
+                'ex'          => $ncm->ex,
+                'NT'          => $ncm->NT,
+                'aliquot'     => $ncm->aliquot,
+                'parent_id'   => $ncm->parent_id,
+            ]);
 
-                $point = QdrantService::makePoint($ncm->id, $vector, [
-                    'ncm_code'    => $ncm->ncm_code,
-                    'description' => $ncm->description,
-                    'ex'          => $ncm->ex,
-                    'NT'          => $ncm->NT,
-                    'aliquot'     => $ncm->aliquot,
-                    'parent_id'   => $ncm->parent_id,
-                ]);
+            $this->qdrantService->upsertPoints('ncm', [$point]);
 
-                $this->qdrantService->upsertPoints('ncm', [$point]);
-
-                $ncm->update([
-                    'embedding_status' => $vector ? 'done' : 'error',
-                ]);
-            } catch (Throwable) {
-                $ncm->update(['embedding_status' => 'error']);
-            }
+            $ncm->update([
+                'embedding_status' => $vector ? 'done' : 'error',
+            ]);
+        } catch (Throwable) {
+            $ncm->update(['embedding_status' => 'error']);
         }
 
         return response()->json(['message' => 'Vectorization process finished.'], 200);
